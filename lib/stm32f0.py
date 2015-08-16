9bfa1723cc3a6b94a415f605f8d502ab7b3fe5b2
Pavel Revak <pavel.revak@gmail.com>
2015-08-16 23:46:23 +0200

    stm32f0 small changes

--- lib/stm32f0.py
+++ lib/stm32f0.py
@@ -17,6 +17,33 @@ class Stm32F0(lib.stm32.Stm32):
     FLASH_SR_BUSY_BIT = 0x00000001
     FLASH_SR_EOP_BIT = 0x00000020
 
+    FLASH_WRITER_OFFSET = lib.stm32.Stm32.SRAM_START
+    FLASH_DATA_OFFSET = lib.stm32.Stm32.SRAM_START + 0x100
+
+    FLASH_WRITER_F0_CODE = [
+        # PARAMS
+        # R0: SRC data
+        # R1: DST data
+        # R2: size
+        # R4: STM32_FLASH_SR
+                    # write:
+        0x03, 0x88, # 0x8803    # ldrh r3, [r0]
+        0x0b, 0x80, # 0x800b    # strh r3, [r1]
+                    # test_busy:
+        0x23, 0x68, # 0x6823    # ldr r3, [r4]
+        0x2b, 0x42, # 0x422b    # tst r3, r5
+        0xfc, 0xd1, # 0xd1fc    # bne <test_busy>
+        0x33, 0x42, # 0x4233    # tst r3, r6
+        0x04, 0xd0, # 0xd104    # beq <exit>
+        0x02, 0x30, # 0x3002    # adds r0, #2
+        0x02, 0x31, # 0x3102    # adds r1, #2
+        0x02, 0x3a, # 0x3a02    # subs r2, #2
+        0x00, 0x2a, # 0x2a00    # cmp r2, #0
+        0xf3, 0xd1, # 0xd1f3    # bne <write>
+                    # exit:
+        0x00, 0xbe, # 0xbe00    # bkpt 0x00
+    ]
+
     def flash_unlock(self):
         self.core_reset_halt()
         # programing locked
@@ -61,33 +88,7 @@ class Stm32F0(lib.stm32.Stm32):
         self._stlink.set_debugreg32(Stm32F0.FLASH_CR_REG, Stm32F0.FLASH_CR_LOCK_BIT)
         self._dbg.bargraph_done()
 
-    FLASH_WRITER_F0_CODE = [
-        # PARAMS
-        # R0: SRC data
-        # R1: DST data
-        # R2: size
-        # R4: STM32_FLASH_BASE
-                    # write:
-        0x03, 0x88, # 0x8803    # ldrh    r3, [r0, #0]
-        0x0b, 0x80, # 0x800b    # strh    r3, [r1, #0]
-                    # test_busy:
-        0xe3, 0x68, # 0x68e3    # ldr r3, [r4, #12]
-        0x2b, 0x42, # 0x422b    # tst r3, r5
-        0xfc, 0xd1, # 0xd1fc    # bne <test_busy>
-        0x33, 0x42, # 0x4233    # tst r3, r6
-        0x04, 0xd0, # 0xd104    # beq <exit>
-        0x02, 0x30, # 0x3002    # adds    r0, #2
-        0x02, 0x31, # 0x3102    # adds    r1, #2
-        0x02, 0x3a, # 0x3a02    # subs    r2, #2
-        0x00, 0x2a, # 0x2a00    # cmp r2, #0
-        0xf3, 0xd1, # 0xd1f3    # bne <write>
-                    # exit:
-        0x00, 0xbe, # 0xbe00    # bkpt    0x00
-    ]
-
     def flash_write(self, addr, data, block_size=1024):
-        FLASH_WRITER_OFFSET = self.SRAM_START
-        FLASH_DATA_OFFSET = self.SRAM_START + 0x100
         if addr is None:
             addr = self.FLASH_START
         if addr % 2:
@@ -98,9 +99,9 @@ class Stm32F0(lib.stm32.Stm32):
             raise lib.stlinkex.StlinkException('Data length is not alligned')
         self._dbg.bargraph_start('Writing FLASH', value_min=addr, value_max=addr + len(data))
         self.flash_unlock()
-        self._stlink.set_mem8(FLASH_WRITER_OFFSET, Stm32F0.FLASH_WRITER_F0_CODE)
+        self._stlink.set_mem8(Stm32F0.FLASH_WRITER_OFFSET, Stm32F0.FLASH_WRITER_F0_CODE)
         # set constants to flash writer
-        self.set_reg('R4', Stm32F0.FLASH_REG_BASE)
+        self.set_reg('R4', Stm32F0.FLASH_SR_REG)
         self.set_reg('R5', Stm32F0.FLASH_SR_BUSY_BIT)
         self.set_reg('R6', Stm32F0.FLASH_SR_EOP_BIT)
         # enable PG
@@ -116,9 +117,9 @@ class Stm32F0(lib.stm32.Stm32):
             if len(block) % 4:
                 block.extend([0xff] * (4 - len(block) % 4))
             # upload data for flashing
-            self._stlink.set_mem32(FLASH_DATA_OFFSET, block)
-            self.set_reg('PC', FLASH_WRITER_OFFSET)
-            self.set_reg('R0', FLASH_DATA_OFFSET)
+            self._stlink.set_mem32(Stm32F0.FLASH_DATA_OFFSET, block)
+            self.set_reg('PC', Stm32F0.FLASH_WRITER_OFFSET)
+            self.set_reg('R0', Stm32F0.FLASH_DATA_OFFSET)
             self.set_reg('R1', addr)
             self.set_reg('R2', len(block))
             # run flash writer
