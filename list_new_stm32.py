2f9e6fed31ff17e7516458174a220d86cf8e0bc1
Pavel Revak <pavel.revak@gmail.com>
2016-05-28 14:28:32 +0200

    added support for new MCUS, fixed SRAM size for some MCUs

--- list_new_stm32.py
+++ list_new_stm32.py
@@ -16,9 +16,40 @@ def fix_cpu_type(cpu_type):
     raise Exception('"%s" is not STM32 family' % cpu_type)
 
 print("Downloading list of all STM32 MCUs from ST.com...")
-res = urllib.request.urlopen('http://www.st.com/stonline/stappl/productcatalog/jsp/jsonDataForL3.jsp?subclassId=1169')
-raw_json = json.loads(res.read().decode('utf-8'))
-mcus = [record for record in raw_json['records']]
+req = urllib.request.urlopen('http://www.st.com/content/st_com/en/products/microcontrollers/stm32-32-bit-arm-cortex-mcus.product-grid.html/SC1169.json')
+res = req.read()
+print("downloaded: %d KBytes" % (len(res) / 1024))
+raw_json = json.loads(res.decode('utf-8'))
+
+# columns has important keys:
+#  - id
+#  - name
+# raw_json['columns']
+REQUESTED_COLUMNS = {
+    'Part Number': 'type',
+    'Core': 'core',
+    'Operating Frequency': 'freq',
+    'FLASH Size': 'flash_size',
+    'Data E2PROM': 'eeprom_size',
+    'Internal RAM Size': 'sram_size',
+}
+
+columns = {}
+for column in raw_json['columns']:
+    if column['name'] in REQUESTED_COLUMNS:
+        columns[column['id']] = REQUESTED_COLUMNS[column['name']]
+
+mcus = []
+for row in raw_json['rows']:
+    mcu = {}
+    mcu['url'] = 'http://www.st.com' + row['productFolderUrl']
+    for cell in row['cells']:
+        column_id = cell['columnId']
+        if column_id in columns:
+            column_name = columns[column_id]
+            mcu[column_name] = cell['value']
+    mcus.append(mcu)
+print("MCUs on ST.com is: %d" % len(mcus))
 
 supported_mcus = {}
 for devs in lib.stm32devices.DEVICES:
@@ -30,58 +61,56 @@ unsupported_mcus = {}
 wrong_param_mcus = {}
 
 for mcu in mcus:
-    m = fix_cpu_type(mcu.get('XJE010^VT-007!0'))
-    core = mcu.get('STP00920^VT-007!0')
-    freq = int(mcu.get('XJG535^VT-007!45'))
-    flash = mcu.get('STP279^VT-007!24')
-    flash = 0 if flash == '-' else int(flash)
-    sram = mcu.get('XJG510^VT-007!0')
-    if sram.isnumeric():
-        sram = int(sram)
-    eeprom = mcu.get('STP681^VT-003!0')
-    eeprom = 0 if eeprom == '-' else int(eeprom) // 1024
-    url = 'http://www.st.com/' + mcu.get('URL')
-    if m in supported_mcus:
-        smcu = supported_mcus[m]
+    mcu_type_fixed = fix_cpu_type(mcu['type'])
+    mcu_type = mcu.get('type')
+    core = mcu.get('core')
+    freq = int(mcu.get('freq'))
+    flash_size = int(mcu.get('flash_size'))
+    sram_size = mcu.get('sram_size')
+    if sram_size.isnumeric():
+        sram_size = int(sram_size)
+    eeprom_size = mcu.get('eeprom_size')
+    eeprom_size = int(eeprom_size) / 1024 if eeprom_size else 0
+    if eeprom_size % 1 == 0:
+        eeprom_size = int(eeprom_size)
+    mcu['eeprom_size'] = eeprom_size
+    if mcu_type_fixed in supported_mcus:
+        supported_mcu = supported_mcus[mcu_type_fixed]
         ok = True
-        if smcu['freq'] != freq:
+        if supported_mcu['freq'] != freq:
             ok = False
-        if smcu['flash_size'] != flash:
+        if supported_mcu['flash_size'] != flash_size:
             ok = False
-        if smcu['sram_size'] != sram:
+        if supported_mcu['sram_size'] != sram_size:
             ok = False
-        if smcu['eeprom_size'] != eeprom:
+        if supported_mcu['eeprom_size'] != eeprom_size:
             ok = False
         if not ok:
-            wrong_param_mcus[m] = {
-                'core': core,
-                'freq': freq,
-                'flash_size': flash,
-                'sram_size': sram,
-                'eeprom_size': eeprom,
-                'url': url,
-                'supported_mcu': smcu,
-            }
+            mcu['supported_mcu'] = supported_mcu
+            mcu['type_fixed'] = mcu_type_fixed
+            wrong_param_mcus[mcu_type] = mcu
     else:
-        unsupported_mcus[m] = {
-            'core': core,
-            'freq': freq,
-            'flash_size': flash,
-            'sram_size': sram,
-            'eeprom_size': eeprom,
-            'url': url,
-        }
+        mcu['type_fixed'] = mcu_type_fixed
+        unsupported_mcus[mcu_type] = mcu
 
 print("%-15s %-15s %6s %6s %6s %6s" % ('type', 'core', 'flash', 'sram', 'eeprom', 'freq'))
 if unsupported_mcus:
-    print('---- unsupported mcus ----')
+    print('---- unsupported mcus (%d) ----' % len(unsupported_mcus))
     for k in sorted(unsupported_mcus.keys()):
         v = unsupported_mcus[k]
-        print("%-15s %-15s %6s %6s %6s %6s %s" % (k, v['core'], v['flash_size'], v['sram_size'], v['eeprom_size'], v['freq'], v['url']))
+        print(v['url'])
+        print("%-15s %-15s %6s %6s %6s %6s" % (
+            k, v['core'], v['flash_size'], v['sram_size'], v.get('eeprom_size'), v['freq']
+        ))
 if wrong_param_mcus:
-    print('---- mcus with wrong params ----')
+    print('---- mcus with wrong params (%d) ----' % len(wrong_param_mcus))
     for k in sorted(wrong_param_mcus.keys()):
         v = wrong_param_mcus[k]
         s = v['supported_mcu']
-        print("%-15s %-15s %6s %6s %6s %6s %s" % (k, v['core'], v['flash_size'], v['sram_size'], v['eeprom_size'], v['freq'], v['url']))
-        print("%-15s %-15s %6s %6s %6s %6s" % ('...', v['core'], s['flash_size'], s['sram_size'], s['eeprom_size'], s['freq']))
+        print(v['url'])
+        print("%-15s %-15s %6s %6s %6s %6s" % (
+            k, v['core'], v['flash_size'], v['sram_size'], v.get('eeprom_size'), v['freq']
+        ))
+        print("%-15s %-15s %6s %6s %6s %6s" % (
+            '(pystlink)', v['core'], s['flash_size'], s['sram_size'], s.get('eeprom_size'), s['freq']
+        ))
