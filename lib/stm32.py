47f733de98cb3a1657906d62240fa758f45e85f2
Uwe Bonnes <bon@elektron.ikp.physik.tu-darmstadt.de>
2019-01-26 15:26:45 +0100

    Force halt.
    Halt will only succeed when the device is running. When devices are mostly
    in WFI, hammer the halt command in a tight loop until we hoepfully hit the
    device in a moment awake.

--- lib/stm32.py
+++ lib/stm32.py
@@ -17,14 +17,17 @@ class Stm32():
     AIRCR_SYSRESETREQ = AIRCR_KEY | AIRCR_SYSRESETREQ_BIT
 
     DHCSR_KEY = 0xa05f0000
-    DHCSR_DEBUGEN_BIT = 0x00000001
-    DHCSR_HALT_BIT = 0x00000002
-    DHCSR_STEP_BIT = 0x00000004
-    DHCSR_STATUS_HALT_BIT = 0x00020000
+    DHCSR_DEBUGEN_BIT       = 0x00000001
+    DHCSR_HALT_BIT          = 0x00000002
+    DHCSR_STEP_BIT          = 0x00000004
+    DHCSR_STATUS_ENABLE_BIT = DHCSR_DEBUGEN_BIT << 16
+    DHCSR_STATUS_HALT_BIT   = DHCSR_HALT_BIT    << 16
     DHCSR_DEBUGDIS = DHCSR_KEY
     DHCSR_DEBUGEN = DHCSR_KEY | DHCSR_DEBUGEN_BIT
     DHCSR_HALT = DHCSR_KEY | DHCSR_DEBUGEN_BIT | DHCSR_HALT_BIT
     DHCSR_STEP = DHCSR_KEY | DHCSR_DEBUGEN_BIT | DHCSR_STEP_BIT
+    DHCSR_HALTED = DHCSR_HALT_BIT | DHCSR_DEBUGEN_BIT |\
+                   DHCSR_STATUS_HALT_BIT | DHCSR_STATUS_ENABLE_BIT
 
     DEMCR_RUN_AFTER_RESET = 0x00000000
     DEMCR_HALT_AFTER_RESET = 0x00000001
@@ -166,7 +169,15 @@ class Stm32():
 
     def core_halt(self):
         self._dbg.debug('Stm32.core_halt()')
-        self._stlink.set_debugreg32(Stm32.DHCSR_REG, Stm32.DHCSR_HALT)
+        verbose = self._dbg._verbose
+        if verbose > 2:
+             self._dbg.set_verbose(2)
+        i = 0
+        while((self._stlink.get_debugreg32(Stm32.DHCSR_REG) & Stm32.DHCSR_HALTED) != Stm32.DHCSR_HALTED) :
+            self._stlink.set_debugreg32(Stm32.DHCSR_REG, Stm32.DHCSR_HALT)
+            i += 1
+        self._dbg.set_verbose(verbose)
+        self._dbg.debug("Halted after %d transactions" % i)
 
     def core_step(self):
         self._dbg.debug('Stm32.core_step()')
